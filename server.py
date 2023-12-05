982a264db4bc550adc86d5114256a8516e7e56fb
vladimirsh <pantene742@gmail.com>
2023-12-05 20:22:18 +0200

    small refactoring

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from sqlDriver import init_sql_connection, close_sql_connection
-import dbQueries
+import queries
 import helpers
 
 app = Flask(__name__)
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route("/users")
 def users():
     cursor = init_sql_connection()
-    cursor.execute(dbQueries.get_all_users())
+    cursor.execute(queries.get_all_users())
     result = helpers.db_users_to_json(cursor.fetchall())
     return result, 200
 
@@ -18,7 +18,7 @@ def users():
 def users_by_id(id=0):
     try:
         cursor = init_sql_connection()
-        cursor.execute(dbQueries.get_user_by_id(id))
+        cursor.execute(queries.get_user_by_id(id))
         result = cursor.fetchall()
         if len(result) != 0:
             return helpers.db_users_to_json(result)
@@ -34,9 +34,9 @@ def create_user():
     try:
         cursor = init_sql_connection()
         user_name = request.get_json()['user_name']
-        create_sql_query = dbQueries.create_user(user_name)
+        create_sql_query = queries.create_user(user_name)
         cursor.execute(create_sql_query)
-        cursor.execute(dbQueries.last_added_user())
+        cursor.execute(queries.last_added_user())
         return helpers.db_users_to_json(cursor.fetchall()), 200
     except:
         return 'Something went wrong', 500
@@ -46,13 +46,13 @@ def create_user():
 def update_user(id):
     try:
         cursor = init_sql_connection()
-        cursor.execute(dbQueries.get_user_by_id(id))
+        cursor.execute(queries.get_user_by_id(id))
         result = cursor.fetchall()
         user_new_name = request.get_json()['user_name']
         if len(result) != 0 and len(user_new_name) > 0:
-            cursor.execute(dbQueries.update_user_by_id(id, user_new_name))
+            cursor.execute(queries.update_user_by_id(id, user_new_name))
             cursor.fetchall()
-            cursor.execute(dbQueries.get_user_by_id(id))
+            cursor.execute(queries.get_user_by_id(id))
             new_user_bd_result = cursor.fetchall()
             return helpers.db_users_to_json(new_user_bd_result), 200
         else:
@@ -65,10 +65,10 @@ def update_user(id):
 def delete_user(id):
     try:
         cursor = init_sql_connection()
-        cursor.execute(dbQueries.get_user_by_id(id))
+        cursor.execute(queries.get_user_by_id(id))
         result = cursor.fetchall()
         if len(result) != 0:
-            cursor.execute(dbQueries.delete_user_by_id(id))
+            cursor.execute(queries.delete_user_by_id(id))
             return id, 200
         else:
             return 'User not found', 404
